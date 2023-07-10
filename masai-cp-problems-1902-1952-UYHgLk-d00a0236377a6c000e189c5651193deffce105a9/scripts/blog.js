2c257a85c300d3b59c6b971a760798be7b4c2479
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-07-10 20:38:33 +0530

    master

--- masai-cp-problems-1902-1952-UYHgLk-d00a0236377a6c000e189c5651193deffce105a9/scripts/blog.js
+++ masai-cp-problems-1902-1952-UYHgLk-d00a0236377a6c000e189c5651193deffce105a9/scripts/blog.js
@@ -24,11 +24,7 @@
 // });
 // // --- do not touch  ↑↑↑↑↑↑↑↑↑↑↑↑ ----------
 
-
-
-// add your js code for blog page here 
-
-// code for time and date
+// Code for time and date
 const date = document.getElementById("date");
 const time = document.getElementById("time");
 
@@ -48,43 +44,18 @@ const currentTime = currentDate.toLocaleTimeString("en-US", {
 
 time.innerText = currentTime;
 
-// back button code
+// Back button code
 document.getElementById("backBtn").addEventListener("click", () => {
   window.location.href = "index.html";
 });
-// --- do not touch  ↑↑↑↑↑↑↑↑↑↑↑↑ ----------
 
-// Problem 7: Able to read blogs using the Read more button
-function getBlogFromLocalStorage() {
-  const blogData = localStorage.getItem("blog");
-  if (blogData) {
-    return JSON.parse(blogData);
-  }
-  return null;
-}
+// Retrieve blog data from localStorage
+const blogData = JSON.parse(localStorage.getItem("blog"));
 
-function renderBlogDetails(blog) {
-  const titleElement = document.querySelector(".title");
-  const subTitleElement = document.querySelector(".sub_title");
-  const profilePicElement = document.querySelector(".profile_pic");
-  const nameElement = document.querySelector(".name");
-  const imageElement = document.querySelector(".image");
-  const descriptionElement = document.querySelector(".description");
-
-  titleElement.innerText = blog.title;
-  subTitleElement.innerText = blog.sub_title;
-  profilePicElement.src = blog.author.profile_pic;
-  nameElement.innerText = blog.author.name;
-  imageElement.src = blog.image;
-  descriptionElement.innerText = blog.description;
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  const blog = getBlogFromLocalStorage();
-  if (blog) {
-    renderBlogDetails(blog);
-  } else {
-    // Handle the case when the blog data is not available
-    console.error("Blog data not found.");
-  }
-});
+// Update DOM with blog data
+document.querySelector(".title").innerText = blogData.title;
+document.querySelector(".sub_title").innerText = blogData.sub_title;
+document.querySelector(".profile_pic").src = blogData.author.profile_pic;
+document.querySelector(".name").innerText = blogData.author.name;
+document.querySelector(".image").src = blogData.image;
+document.querySelector(".description").innerText = blogData.description;
--- masai-cp-problems-1902-1952-UYHgLk-d00a0236377a6c000e189c5651193deffce105a9/scripts/index.js
+++ masai-cp-problems-1902-1952-UYHgLk-d00a0236377a6c000e189c5651193deffce105a9/scripts/index.js
@@ -45,7 +45,7 @@
 
 
 // // add your js code for index page here 
-// code for time and date 
+// Code for time and date
 const date = document.getElementById("date");
 const time = document.getElementById("time");
 
@@ -58,76 +58,95 @@ const currentDate2 = new Date().toLocaleDateString("en-US", {
 date.innerText = currentDate2;
 
 const currentDate = new Date();
-const currentTime = currentDate.toLocaleTimeString('en-US', {
-  hour: '2-digit',
-  minute: '2-digit',
-  hour12: true
+const currentTime = currentDate.toLocaleTimeString("en-US", {
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: true,
 });
 
-time.innerText = currentTime
+time.innerText = currentTime;
 
-// --- do not touch  ↓↓↓↓↓↓↓↓↓↓↓↓ ----------
-const baseServerURL = `http://localhost:9090/`;
-// --- do not touch  ↑↑↑↑↑↑↑↑↑↑↑↑ ----------
-
-// ***** Constants / Variables ***** //
+// Constants / Variables
+const baseServerURL = "http://localhost:3000"; // Update the server URL if necessary
 const blogsURL = `${baseServerURL}/blogs`;
 let mainSection = document.getElementById("data-list-wrapper");
 
 let blogsData = [];
 
-// Problem 1: Shows the correct Welcome title
-const welcomeTitle = document.createElement("h2");
-welcomeTitle.innerText = "Welcome, Peter";
-document.getElementById("welcome").appendChild(welcomeTitle);
+// Fetch blogs data from the server
+async function fetchBlogsData() {
+  try {
+    const response = await fetch(blogsURL);
+    const data = await response.json();
+    blogsData = data;
+    console.log(blogsData);
+    displayBlogsData(blogsData);
+  } catch (error) {
+    console.error("Error fetching blogs data:", error);
+  }
+}
 
-// Problem 2: List of blogs on page load
-function renderBlogCard(blog) {
+// Display blogs data in the DOM
+function displayBlogsData(blogs) {
+  mainSection.innerHTML = ""; // Clear previous data
+
+  blogs.forEach((blog) => {
+    const card = createBlogCard(blog);
+    mainSection.appendChild(card);
+  });
+}
+
+// Create a blog card element
+function createBlogCard(blog) {
   const card = document.createElement("div");
-  card.className = "card";
+  card.classList.add("card");
 
   const cardImg = document.createElement("div");
-  cardImg.className = "card_img";
-  // Set image of the blog using blog.image
+  cardImg.classList.add("card_img");
+  // Add blog image logic here (e.g., cardImg.style.backgroundImage = `url(${blog.image})`;)
 
   const cardBody = document.createElement("div");
-  cardBody.className = "card_body";
+  cardBody.classList.add("card_body");
 
   const title = document.createElement("p");
-  title.className = "title";
+  title.classList.add("title");
   title.innerText = blog.title;
 
   const subTitle = document.createElement("p");
-  subTitle.className = "sub_title";
+  subTitle.classList.add("sub_title");
   subTitle.innerText = blog.sub_title;
 
   const category = document.createElement("p");
-  category.className = "category";
+  category.classList.add("category");
   category.innerText = blog.category;
 
   const profilePic = document.createElement("img");
-  profilePic.className = "profile_pic";
-  // Set profile picture using blog.author.profile_pic
+  profilePic.classList.add("profile_pic");
+  profilePic.src = blog.author.profile_pic;
 
   const name = document.createElement("p");
-  name.className = "name";
+  name.classList.add("name");
   name.innerText = blog.author.name;
 
   const publishDate = document.createElement("p");
-  publishDate.className = "publish_date";
+  publishDate.classList.add("publish_date");
   publishDate.innerText = blog.publish_date;
 
   const deleteBtn = document.createElement("button");
-  deleteBtn.className = "deleteBtn";
+  deleteBtn.classList.add("deleteBtn");
   deleteBtn.innerText = "Delete";
   deleteBtn.setAttribute("data-id", blog.id);
-  deleteBtn.addEventListener("click", () => deleteBlog(blog.id));
+  deleteBtn.addEventListener("click", () => {
+    deleteBlog(blog.id);
+  });
 
   const readBtn = document.createElement("button");
-  readBtn.className = "readBtn";
+  readBtn.classList.add("readBtn");
   readBtn.innerText = "Read More";
   readBtn.setAttribute("data-id", blog.id);
-  readBtn.addEventListener("click", () => redirectToBlogPage(blog));
+  readBtn.addEventListener("click", () => {
+    viewBlog(blog);
+  });
 
   cardBody.appendChild(title);
   cardBody.appendChild(subTitle);
@@ -144,87 +163,60 @@ function renderBlogCard(blog) {
   return card;
 }
 
-function renderBlogCards() {
-  mainSection.innerHTML = ""; // Clear existing blog cards
-
-  blogsData.forEach((blog) => {
-    const card = renderBlogCard(blog);
-    mainSection.appendChild(card);
-  });
-}
-
-async function fetchBlogs() {
-  try {
-    const response = await fetch(blogsURL);
-    const data = await response.json();
-    blogsData = data;
-    renderBlogCards();
-  } catch (error) {
-    console.error("Error fetching blogs:", error);
-  }
-}
-
-// Call the fetchBlogs function on page load
-window.addEventListener("load", fetchBlogs);
-
-// Problem 3: Ability to delete a Blog
+// Delete a blog
 async function deleteBlog(blogId) {
   try {
     await fetch(`${blogsURL}/${blogId}`, {
       method: "DELETE",
     });
-    // Update the blogsData array by removing the deleted blog
-    blogsData = blogsData.filter((blog) => blog.id !== blogId);
-    renderBlogCards();
+    fetchBlogsData(); // Refresh blogs data after deletion
   } catch (error) {
     console.error("Error deleting blog:", error);
   }
 }
 
-// Problem 4: Check sort the Newest first functionality working as expected
-newestFirst.addEventListener("click", () => {
-  blogsData.sort((a, b) => new Date(b.publish_date) - new Date(a.publish_date));
-  renderBlogCards();
-});
-
-// Problem 5: Check sort Oldest first functionality working as expected
-oldestFirst.addEventListener("click", () => {
-  blogsData.sort((a, b) => new Date(a.publish_date) - new Date(b.publish_date));
-  renderBlogCards();
-});
-
-// Problem 6: Filtering Blogs based on the category
-function filterBlogsByCategory(category) {
-  if (category === "All") {
-    renderBlogCards();
-  } else {
-    const filteredBlogs = blogsData.filter((blog) => blog.category === category);
-    renderBlogCards(filteredBlogs);
-  }
-}
-
-filterInternational.addEventListener("click", () => {
-  filterBlogsByCategory("International");
-});
-
-filterBusiness.addEventListener("click", () => {
-  filterBlogsByCategory("Business");
-});
-
-filterEntertainment.addEventListener("click", () => {
-  filterBlogsByCategory("Entertainment");
-});
-
-filterSports.addEventListener("click", () => {
-  filterBlogsByCategory("Sports");
-});
-
-filterHealth.addEventListener("click", () => {
-  filterBlogsByCategory("Health");
-});
-
-// Problem 7: Able to read blogs using the Read more button
-function redirectToBlogPage(blog) {
+// View a blog
+function viewBlog(blog) {
   localStorage.setItem("blog", JSON.stringify(blog));
   window.location.href = "blog.html";
 }
+
+// Sort blogs by newest first
+function sortNewestFirst() {
+  blogsData.sort((a, b) => new Date(b.publish_date) - new Date(a.publish_date));
+  displayBlogsData(blogsData);
+}
+
+// Sort blogs by oldest first
+function sortOldestFirst() {
+  blogsData.sort((a, b) => new Date(a.publish_date) - new Date(b.publish_date));
+  displayBlogsData(blogsData);
+}
+
+// Filter blogs by category
+function filterByCategory(category) {
+  const filteredBlogs = blogsData.filter((blog) => blog.category === category);
+  displayBlogsData(filteredBlogs);
+}
+
+// Event Listeners
+document.getElementById("newest-first").addEventListener("click", sortNewestFirst);
+document.getElementById("oldest-first").addEventListener("click", sortOldestFirst);
+document.getElementById("filter-International").addEventListener("click", () => {
+  filterByCategory("International");
+});
+document.getElementById("filter-Business").addEventListener("click", () => {
+  filterByCategory("Business");
+});
+document.getElementById("filter-Entertainment").addEventListener("click", () => {
+  filterByCategory("Entertainment");
+});
+document.getElementById("filter-Sports").addEventListener("click", () => {
+  filterByCategory("Sports");
+});
+document.getElementById("filter-Health").addEventListener("click", () => {
+  filterByCategory("Health");
+});
+
+// Fetch blogs data on page load
+fetchBlogsData();
